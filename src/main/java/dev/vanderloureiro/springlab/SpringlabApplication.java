b64b8976d45c66ef6c11c8f5e0cf9ea9f1c1f46b
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-04-04 18:43:45 -0300

    start do projeto

---
+++ src/main/java/dev/vanderloureiro/springlab/SpringlabApplication.java
@@ -0,0 +1,13 @@
+package dev.vanderloureiro.springlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vanderloureiro/springlab/SpringlabApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vanderloureiro.springlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
